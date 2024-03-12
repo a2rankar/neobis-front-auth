a01227cf90aeb6bbc8e9b29951501a9b7e30105a
Umar <umarsabyrbekov123@gmail.com>
2024-03-12 14:40:53 +0600

    add validation

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -7,12 +7,12 @@ import { Link } from "react-router-dom";
 // import getToken from "../ApiServer/token";
 
 
-// import logo from "./logo.png"; 
+
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
-    // const [token, setToken] = useState("");
+    const [token, setToken] = useState("");
 
    
     
@@ -30,16 +30,16 @@ function Login() {
             }
         };
 
-        async function getToken() {
-            try {
-                const response = await axios.get('https://royal-nerve-lorby.up.railway.app/api/auth/token');
-                const token = response.data.token;
-                return token;
-            } catch (error) {
-                console.error('Ошибка при получении токена:', error);
-                throw error;
-            }
-        }
+        // async function getToken() {
+        //     try {
+        //         const response = await axios.get('https://royal-nerve-lorby.up.railway.app/api/auth/token');
+        //         const token = response.data.token;
+        //         return token;
+        //     } catch (error) {
+        //         console.error('Ошибка при получении токена:', error);
+        //         throw error;
+        //     }
+        // }
         
     
 
@@ -48,33 +48,34 @@ function Login() {
            
             <form onSubmit={handleLogin} className="login-form">
                 <div className="input-forms">
-                    <div>
-                       
-                        <input 
-                            type="text" 
-                            id="username" 
-                            name="username" 
-                            placeholder="vvedy suda-tuda login"
-                            value={username} 
-                            onChange={(e) => setUsername(e.target.value)} 
-                        />
-                    </div>
-                    <div>
-                     
-                        <input 
-                            type={showPassword ? "text" : "password"} 
-                            id="password" 
-                            name="password" 
-                            value={password} 
-                            onChange={(e) => setPassword(e.target.value)} 
-                        />
-                           <FontAwesomeIcon 
-                    icon={showPassword ? faEye : faEyeSlash} 
-                    onClick={() => setShowPassword(!showPassword)} 
-                    className="password-toggle-eye"
-                />
+                        <div>
                         
-                    </div>
+                            <input 
+                                type="text" 
+                                id="username" 
+                                name="username" 
+                                placeholder="vvedy suda-tuda login"
+                                value={username} 
+                                onChange={(e) => setUsername(e.target.value)} 
+                            />
+                        </div>
+                        <div>
+                        
+                            <input 
+                                type={showPassword ? "text" : "password"} 
+                                id="password" 
+                                name="password" 
+                                placeholder="enter a password"
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)} 
+                            />
+                            <FontAwesomeIcon 
+                        icon={showPassword ? faEye : faEyeSlash} 
+                        onClick={() => setShowPassword(!showPassword)} 
+                        className="password-toggle-eye"
+                    />
+                        
+                        </div>
                 </div>    
                 <button type="submit" className="Login-button">Войти</button>
             </form>
--- src/components/Auth/RegistrationForm.js
+++ src/components/Auth/RegistrationForm.js
@@ -5,6 +5,8 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Registration() {
+    const [passwordValid, setPasswordValid] = useState(true);
+    const [confirmPasswordValid, setConfirmPasswordValid] = useState(true);
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -14,7 +16,20 @@ function Registration() {
 
     const handleRegistration = async (e) => {
         e.preventDefault();
-    
+
+        // Проверка валидации пароля и его подтверждения
+        const isPasswordValid = password.length >= 8 && /\d/.test(password) && /[!@#$%^&*]/.test(password);
+        const isConfirmPasswordValid = confirmPassword === password;
+
+        // Обновление состояния валидации
+        setPasswordValid(isPasswordValid);
+        setConfirmPasswordValid(isConfirmPasswordValid);
+
+        // Если пароль или его подтверждение неверны, прекращаем регистрацию
+        if (!isPasswordValid || !isConfirmPasswordValid) {
+            return;
+        }
+
         try {
             const response = await axios.post('https://royal-nerve-lorby.up.railway.app/api/auth/register', {
                 email,
@@ -26,17 +41,15 @@ function Registration() {
 
         } catch (error) {
             console.error('Ошибка регистрации:', error);
-          
         }
     };
 
     return (
         <div className="container">
-         
             <form onSubmit={handleRegistration}>
                 <div>
-                  
-                    <input placeholder="Введи адрес почты:"
+                    <input 
+                        placeholder="Введи адрес почты:"
                         type="email" 
                         id="email" 
                         name="email" 
@@ -45,8 +58,8 @@ function Registration() {
                     />
                 </div>
                 <div>
-                 
-                    <input placeholder="Pridumay Login"
+                    <input 
+                        placeholder="Придумайте логин"
                         type="text" 
                         id="username" 
                         name="username" 
@@ -55,39 +68,42 @@ function Registration() {
                     />
                 </div>
                 <div>
-                  
-                    <input placeholder="Create a new password"
+                    <input 
+                        placeholder="Введите новый пароль"
                         type={showPassword ? "text" : "password"} 
                         id="password" 
                         name="password" 
                         value={password} 
-                        onChange={(e) => setPassword(e.target.value)} 
+                        onChange={(e) => setPassword(e.target.value)}
+                        className={passwordValid ? 'password-input valid-password' : 'password-input invalid-password'}  
                     />
-                     <FontAwesomeIcon 
+                    <FontAwesomeIcon 
                         icon={showPassword ? faEye : faEyeSlash} 
                         onClick={() => setShowPassword(!showPassword)} 
                         className="password-toggle-eye"
                     />
+                    {!passwordValid && <p className="error-message">Пароль должен содержать минимум 8 символов, хотя бы 1 цифру и 1 специальный символ</p>}
                 </div>
                 <div>
-                   
-                    <input placeholder="confirm your password"
+                    <input 
+                        placeholder="Подтвердите пароль"
                         type={showConfirmPassword ? "text" : "password"} 
                         id="confirmPassword" 
                         name="confirmPassword" 
                         value={confirmPassword} 
                         onChange={(e) => setConfirmPassword(e.target.value)} 
+                        className={confirmPasswordValid ? 'password-input valid-password' : 'password-input invalid-password'}  
                     />
-                      <FontAwesomeIcon 
-                        icon={showPassword ? faEye : faEyeSlash} 
-                        onClick={() => setShowPassword(!showPassword)} 
+                    <FontAwesomeIcon 
+                        icon={showConfirmPassword ? faEye : faEyeSlash} 
+                        onClick={() => setShowConfirmPassword(!showConfirmPassword)} 
                         className="password-toggle-eye"
                     />
+                    {!confirmPasswordValid && <p className="error-message">Пароли не совпадают</p>}
                 </div>
                 <button type="submit">Зарегистрироваться</button>
-                
             </form>
-            <p>Уже зарегистрированы? <a href="/login">Авторизация</a></p>
+            <p>Уже зарегистрированы? <a href="/">Авторизация</a></p>
         </div>
     );
 }
